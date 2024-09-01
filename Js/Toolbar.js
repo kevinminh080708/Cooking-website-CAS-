d5119d492263f900aab6a4fc7c3991e2d35069a9
Leo <hunglolvcl123!@gmail.com>
2024-09-01 18:38:07 +0700

    add search bar and logo

--- Js/Toolbar.js
+++ Js/Toolbar.js
@@ -4,14 +4,35 @@
 function injectHtmlContent() {
     // Define the HTML content you want to inject
     const newContent = `
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>search</title>
+    <link rel="stylesheet" href="/Css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<body>
+</head>
+<body>
     <div class="topbar">
                 <a href="/Public/index.html">Home</a>
                 <a href="/Html/catergories.html">Catergories</a>
                 <a href="/Html/specificrecipe.html">Specific Recipe</a>
                 <a href="/Html/about.html">About</a>
                 <a href="/Html/faq.html">FaQ</a>
-                <a href="/Html/helpandservice.html">Help service</a>
+                <a href="/Html/helpandservice.html">Help service</a>               
+                <img class="logo " src="../picture/weblogo.png" alt="logo">
+                <div class="search">
+                    <form>
+                        <input type="text" name="" placeholder="Search. . . ">
+                        <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+                    </form>
+                </div>
+
              </div>
+    </body>
+</html>
     `;
 
     // Find the placeholder element in the document
